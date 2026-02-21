e6319171af20de842e4d03d4ec1ed3698b1d5963
Shreyas Kundaragi <shreyaskundaragi@gmail.com>
2026-02-20 21:29:33 -0600

    setup

---
+++ Account.cs
@@ -0,0 +1,52 @@
+class Account
+{
+    float amount;
+    string accountUser;
+
+
+    public Account(float amount, string accountUser)
+    {
+        this.amount = amount;
+        this.accountUser = accountUser;
+    }
+
+
+    public void show()
+    {
+        Console.WriteLine($"User: {accountUser} Balance: {amount}");
+    }
+
+
+    public void deposit(float amount)
+    {
+        this.amount += amount;
+    }
+
+
+    public float getBalance()
+    {
+        return amount;
+    }
+
+
+    public bool withdraw(float amount)
+    {
+        if (this.amount >= amount)
+        {
+            this.amount -= amount;
+            return true;
+        }
+        return false;
+    }
+
+
+    public bool transfer(float amount, Account other)
+    {
+        if (this.amount >= amount)
+        {
+            other.amount += amount;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Capstone_assignment;
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Account account1 = new Account(100, "Bob");
+        Account account2 = new Account(100, "Mary");
+
+
+
+
+        account1.show();
+        if (account1.withdraw(150))
+        {
+            Console.WriteLine("The withdraw was successful");
+            Console.WriteLine("----------------------------");
+        }
+        else
+        {
+            Console.WriteLine("The withdraw was unsuccessful");
+            Console.WriteLine("----------------------------");
+        }
+        account1.deposit(200);
+        Console.WriteLine($"Deposited $200 to Bob");
+        account1.show();
+        if (account1.withdraw(150))
+        {
+            Console.WriteLine("The withdraw was successful");
+            Console.WriteLine("----------------------------");
+        }
+        else
+        {
+            Console.WriteLine("The withdraw was unsuccessful");
+            Console.WriteLine("----------------------------");
+        }
+        account1.show();
+
+
+
+
+        if (account1.transfer(50, account1))
+        {
+            Console.WriteLine("Transfered $50 to Bob");
+            Console.WriteLine("----------------------------");
+        }
+        else
+        {
+            Console.WriteLine("Transfer failed");
+            Console.WriteLine("----------------------------");
+        }
+        account1.show();
+        account2.show();
+    }
+}
